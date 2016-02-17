95b0969c4719f6606bba3ca7f371ce64094ad3b8
Simon Markey <simon.markey@buy4now.ie>
2016-02-17 15:14:28 +0000

    Upgrading to 4.5.2 as 4.6 has issues until 4.6.1 is available

---
+++ OperationMicrosoft/Tests/DeepCopy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using NUnit.Framework;
+
+namespace OperationMicrosoft.Tests
+{
+    public class DeepCopy
+    {
+        private static T DeepCopyCreate<T>(T obj)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, obj);
+                stream.Position = 0;
+
+                return (T) formatter.Deserialize(stream);
+            }
+        }
+
+        [Test]
+        public void Method_Scenario_Result()
+        {
+            var emily = new Person {Name = "Emily"};
+            Person bronteClone = DeepCopyCreate(emily);
+            emily.Name = "Joe";
+            Console.WriteLine(bronteClone.Name);
+        }
+    }
+
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ OperationMicrosoft/Tests/Structer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace OperationMicrosoft.Tests
+{
+    public struct Structer
+    {
+        public readonly int U;
+        private readonly int _u2;
+        public int U3 { get; set; }
+
+        public int U2
+        {
+            get
+            {
+                return _u2;
+            }
+        }
+
+        public Structer(int u) : this()
+        {
+            U = u;
+            _u2 = u;
+        }
+    }
+
+    class X
+    {
+        protected virtual void F() { Console.WriteLine("X.F"); }
+        protected virtual void F2() { Console.WriteLine("X.F2"); }
+    }
+    class Y : X
+    {
+        sealed protected override void F() { Console.WriteLine("Y.F"); }
+        protected override void F2() { Console.WriteLine("Y.F2"); }
+    }
+    class Z : Y
+    {
+        // Attempting to override F causes compiler error CS0239. 
+//         protected override void F() { Console.WriteLine("C.F"); }
+
+        // Overriding F2 is allowed. 
+        protected override void F2() { Console.WriteLine("Z.F2"); }
+    }
+
+
+    public class TestStruct
+    {
+        [Test]
+        public void Method_Scenario_Result()
+        {
+            var structer = new Structer(10);
+
+            Console.WriteLine(structer.U);
+            Console.WriteLine(structer.U2);
+            Console.WriteLine(structer.U3);
+
+//            structer.U = 12;
+//            structer.U2 = 12;
+//            structer.U3 = 12;
+        }
+    }
+}
